bffb58d2fe3626c82ff187aff2903023f09bb1b7
Luca Mattiazzi <luca.mattiazzi@cayenne.it>
2016-03-10 16:00:10 +0100

    migrate and better-errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310145147) do
+ActiveRecord::Schema.define(version: 20160310145425) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.integer  "code",       limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.datetime "date"
+    t.integer  "number",     limit: 4
+    t.integer  "quantity",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
 end
