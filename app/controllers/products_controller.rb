b505ce3fc62840ee8f280f3665908e963b14c73b
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-03-19 01:30:34 +0100

    route fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -63,7 +63,7 @@ class ProductsController < ApplicationController
 
   private
     def set_product
-      @product = Product.find(params[:id])
+      @product = Product.find(params[:code])
     end
 
     def product_params
