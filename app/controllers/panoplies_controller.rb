fcbd754c311d979e02cd2c40a5e1355c1383dd68
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-05-24 01:13:06 +0200

    panoplies ended

--- app/controllers/panoplies_controller.rb
+++ app/controllers/panoplies_controller.rb
@@ -4,5 +4,14 @@ class PanopliesController < ApplicationController
     product_codes = params[:couple].split('_')
     product_ids = product_codes.map{|code| Product.find_by_code(code)[:id]}
     @panoplie = Panoplie.find_by_product_id_1_and_product_id_2(product_ids.first, product_ids.second)
+    decorated = @panoplie.decorate
+    @panoplie_hash = { total_worth: decorated.total_worth,
+                       correlation_factor: decorated.correlation_factor,
+                       perfect_sales_factor: decorated.perfect_sales_factor,
+                       real_probable_factor: decorated.real_probable_factor,
+                       money_factor: decorated.money_factor,
+                       quantity: @panoplie[:quantity],
+                     }
+    @panoplie_hash[:importance] = @panoplie[:importance]
   end
 end
