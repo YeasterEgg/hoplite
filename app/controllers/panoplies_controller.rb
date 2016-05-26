7565beeadde6496f3915af1ecb91eca7d25b3a7f
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-05-26 18:43:20 +0200

    fix

--- app/controllers/panoplies_controller.rb
+++ app/controllers/panoplies_controller.rb
@@ -5,7 +5,6 @@ class PanopliesController < ApplicationController
     product_ids = product_codes.map{|code| Product.find_by_code(code)[:id]}
     @panoplie = Panoplie.find_by_product_id_1_and_product_id_2(product_ids.first, product_ids.second)
     @panoplie_attributes = [
-                            :total_worth,
                             :correlation_factor,
                             :perfect_sales_factor,
                             :real_probable_factor,
